e8cbb654dd36ba771f499249de171498ab7df740
r0ddty <r0d@ukr.net>
2025-01-11 16:40:08 +0200

    hammerhead: use libfmjni-patched vendor/lineage repo
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,6 @@
 git clone -b lineage-21.0 https://github.com/r0ddty/kernel_lge_hammerhead kernel/lge/hammerhead --depth 1
 git clone -b lineage-21.0 https://github.com/r0ddty/vendor_lge vendor/lge --depth 1
+if [ ! vendor/lineage/.mdf ]; then
+    rm -rf vendor/lineage
+    git clone https://github.com/r0ddty/ul-android_vendor_lineage vendor/lineage
+fi
