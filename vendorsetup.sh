0fe99285493111aa318646c2adb24716ffc03910
r0ddty <r0d@ukr.net>
2025-01-11 16:42:05 +0200

    hammerhead: update vendorsetup.sh
    Signed-off-by: r0ddty <r0d@ukr.net>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 git clone -b lineage-21.0 https://github.com/r0ddty/kernel_lge_hammerhead kernel/lge/hammerhead --depth 1
 git clone -b lineage-21.0 https://github.com/r0ddty/vendor_lge vendor/lge --depth 1
-if [ ! vendor/lineage/.mdf ]; then
+if [ ! -f vendor/lineage/.mdf ]; then
+    echo "clonning patched vendor/lineage repo"
     rm -rf vendor/lineage
     git clone https://github.com/r0ddty/ul-android_vendor_lineage vendor/lineage
 fi
