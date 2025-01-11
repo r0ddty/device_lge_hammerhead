ec4864f3fb6da92f501321f4e30db6886550f8b7
r0ddty <r0d@ukr.net>
2025-01-11 15:38:12 +0200

    hammerhead: use vendorsetup.sh
    Signed-off-by: r0ddty <r0d@ukr.net>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone -b lineage-21.0 https://github.com/r0ddty/kernel_lge_hammerhead kernel/lge/hammerhead --depth 1
+git clone -b lineage-21.0 https://github.com/r0ddty/vendor_lge vendor/lge --depth 1
